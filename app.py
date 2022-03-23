9a3abc28e3e79873ab821db88a5825238c7eb44b
yuxhux <39603281+yuxhux@users.noreply.github.com>
2022-03-22 23:12:50 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, flash
 app = Flask(__name__)
 app.secret_key = "manbearpig_MUDMAN888"
 
-@app.route("/hello")
+@app.route("/")
 def index():
 	flash("what's your name?")
 	return render_template("index.html")
